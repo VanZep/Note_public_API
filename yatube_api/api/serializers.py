e403efd8ca2e08b11433323858f6f4ae63c45601
Иван Озеров <vanzup@mail.ru>
2023-05-18 22:41:44 +0300

    second try

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -54,7 +54,7 @@ class FollowSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Follow
-        fields = ('user', 'following', 'id')
+        fields = ('user', 'following')
         validators = [
             UniqueTogetherValidator(
                 queryset=Follow.objects.all(),
